443b975d75cea2d8031b874729dcf966b79fe56c
Kevin <arpaskevin11@gmail.com>
2024-02-08 08:24:13 +0100

    Shooting System Added.

--- Assets/Scripts/Destroy.cs
+++ Assets/Scripts/Destroy.cs
@@ -3,9 +3,5 @@ using UnityEngine;
 public class Destroy : MonoBehaviour
 {
     [SerializeField] private float aliveTime = 3f;
-
-    private void Awake()
-    {
-        Destroy(gameObject, aliveTime);
-    }
+    private void Awake() => Destroy(gameObject, aliveTime);
 }
